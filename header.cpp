c811dc4bbcc9bd75647f853349c19ef910681c6e
3x0c3t <jcalonne.prv@gmail.com>
2026-08-12 19:29:41 +0200

    refactor: split H1B4 into H1B4 H1B5

--- header.cpp
+++ header.cpp
@@ -5,307 +5,260 @@
 #include "display.h"
 #include "settings.h"
 
-// ============================================================
-// OUTIL INTERNE
-// Dessine un bloc avec fond + bordure
-// ============================================================
-
-static void drawBlock(
-  int16_t x,
-  int16_t y,
-  int16_t w,
-  int16_t h,
-  uint16_t bgc,
-  uint16_t bc
-)
-{
-  tft.fillRect(
-    x,
-    y,
-    w,
-    h,
-    bgc
-  );
-
-  tft.drawRect(
-    x,
-    y,
-    w,
-    h,
-    bc
-  );
-}
-
-// ============================================================
-// HEADER 1
-// ============================================================
-
+// H1
 void header1Draw()
 {
-  // ----------------------------------------------------------
-  // H1B1 : HEURE
-  // ----------------------------------------------------------
-
-  drawBlock(
-    H1B1_X,
-    H1B1_Y,
-    H1B1_W,
-    H1B1_H,
-    H1B1_BGC,
-    H1B1_BC
-  );
-
-  tft.setTextSize(H1B1_TEXT_SIZE);
-
-  tft.setTextDatum(MC_DATUM);
-
-  tft.setTextColor(
-    H1B1_TC,
-    H1B1_BGC
-  );
-
-  tft.drawString(
-    "12:34:56",
-    H1B1_X + H1B1_W / 2,
-    H1B1_Y + H1B1_H / 2
-  );
-
-  // ----------------------------------------------------------
-  // H1B2 : DATE
-  // ----------------------------------------------------------
-
-  drawBlock(
-    H1B2_X,
-    H1B2_Y,
-    H1B2_W,
-    H1B2_H,
-    H1B2_BGC,
-    H1B2_BC
-  );
-
-  tft.setTextSize(H1B2_TEXT_SIZE);
-
-  tft.setTextColor(
-    H1B2_TC,
-    H1B2_BGC
-  );
-
-  tft.drawString(
-    "2026:08:12",
-    H1B2_X + H1B2_W / 2,
-    H1B2_Y + H1B2_H / 2
-  );
-
-  // ----------------------------------------------------------
-  // H1B3 : LOGO
-  // ----------------------------------------------------------
-
-  drawBlock(
-    H1B3_X,
-    H1B3_Y,
-    H1B3_W,
-    H1B3_H,
-    H1B3_BGC,
-    H1B3_BC
-  );
-
-  tft.setTextSize(H1B3_TEXT_SIZE);
-
-  tft.setTextColor(
-    H1B3_TC,
-    H1B3_BGC
-  );
-
-  tft.drawString(
-    "-3X0C3T-",
-    H1B3_X + H1B3_W / 2,
-    H1B3_Y + H1B3_H / 2
-  );
-
-  // ----------------------------------------------------------
-  // H1B4 : STATUS
-  // ----------------------------------------------------------
-
-  drawBlock(
-    H1B4_X,
-    H1B4_Y,
-    H1B4_W,
-    H1B4_H,
-    H1B4_BGC,
-    H1B4_BC
-  );
-
-  // ----------------------------------------------------------
-  // H1B4 : CARRES
-  // ----------------------------------------------------------
-
-  const int totalSquaresWidth =
-    (H1B4_SQUARE_COUNT * H1B4_SQUARE_SIZE) +
-    ((H1B4_SQUARE_COUNT - 1) * H1B4_SQUARE_SPACING);
-
-  const int startX =
-    H1B4_X +
-    (H1B4_W - totalSquaresWidth) / 2;
-
-  const int startY =
-    H1B4_Y +
-    (H1B4_H - H1B4_SQUARE_SIZE) / 2;
-
-  for (int i = 0; i < H1B4_SQUARE_COUNT; i++)
-  {
-    const int squareX =
-      startX +
-      i * (H1B4_SQUARE_SIZE + H1B4_SQUARE_SPACING);
-
+    // H1B1
     tft.fillRect(
-      squareX,
-      startY,
-      H1B4_SQUARE_SIZE,
-      H1B4_SQUARE_SIZE,
-      H1B4_SQUARE_BGC
+        H1B1_X,
+        H1B1_Y,
+        H1B1_W,
+        H1B1_H,
+        H1B1_BGC
     );
 
     tft.drawRect(
-      squareX,
-      startY,
-      H1B4_SQUARE_SIZE,
-      H1B4_SQUARE_SIZE,
-      H1B4_SQUARE_BC
+        H1B1_X,
+        H1B1_Y,
+        H1B1_W,
+        H1B1_H,
+        H1B1_BC
     );
-  }
 
-  tft.setTextDatum(TL_DATUM);
+    tft.setTextSize(H1B1_TEXT_SIZE);
+    tft.setTextDatum(MC_DATUM);
+    tft.setTextColor(
+        H1B1_TC,
+        H1B1_BGC
+    );
+
+    tft.drawString(
+        "12:34:56",
+        H1B1_X + H1B1_W / 2,
+        H1B1_Y + H1B1_H / 2
+    );
+
+    // H1B2
+    tft.fillRect(
+        H1B2_X,
+        H1B2_Y,
+        H1B2_W,
+        H1B2_H,
+        H1B2_BGC
+    );
+
+    tft.drawRect(
+        H1B2_X,
+        H1B2_Y,
+        H1B2_W,
+        H1B2_H,
+        H1B2_BC
+    );
+
+    tft.setTextSize(H1B2_TEXT_SIZE);
+    tft.setTextColor(
+        H1B2_TC,
+        H1B2_BGC
+    );
+
+    tft.drawString(
+        "2026/08/12",
+        H1B2_X + H1B2_W / 2,
+        H1B2_Y + H1B2_H / 2
+    );
+
+    // H1B3
+    tft.fillRect(
+        H1B3_X,
+        H1B3_Y,
+        H1B3_W,
+        H1B3_H,
+        H1B3_BGC
+    );
+
+    tft.drawRect(
+        H1B3_X,
+        H1B3_Y,
+        H1B3_W,
+        H1B3_H,
+        H1B3_BC
+    );
+
+    tft.setTextSize(H1B3_TEXT_SIZE);
+    tft.setTextColor(
+        H1B3_TC,
+        H1B3_BGC
+    );
+
+    tft.drawString(
+        "-3X0C3T-",
+        H1B3_X + H1B3_W / 2,
+        H1B3_Y + H1B3_H / 2
+    );
+
+    // H1B4
+    tft.fillRect(
+        H1B4_X,
+        H1B4_Y,
+        H1B4_W,
+        H1B4_H,
+        H1B4_BGC
+    );
+
+    tft.drawRect(
+        H1B4_X,
+        H1B4_Y,
+        H1B4_W,
+        H1B4_H,
+        H1B4_BC
+    );
+
+    // H1B5
+    tft.fillRect(
+        H1B5_X,
+        H1B5_Y,
+        H1B5_W,
+        H1B5_H,
+        H1B5_BGC
+    );
+
+    tft.drawRect(
+        H1B5_X,
+        H1B5_Y,
+        H1B5_W,
+        H1B5_H,
+        H1B5_BC
+    );
+
+    tft.setTextDatum(TL_DATUM);
 }
 
-// ============================================================
-// HEADER 2
-// ============================================================
-
+// H2
 void header2Draw(const char *title)
 {
-  // ----------------------------------------------------------
-  // H2B1 : MENU
-  // ----------------------------------------------------------
+    // H2 fond
+    tft.fillRect(
+        0,
+        H2_Y,
+        SCREEN_WIDTH,
+        H2_H,
+        TFT_BLACK
+    );
 
-  drawBlock(
-    H2B1_X,
-    H2B1_Y,
-    H2B1_W,
-    H2B1_H,
-    H2B1_BGC,
-    H2B1_BC
-  );
+    // H2B1
+    tft.fillRect(
+        H2B1_X,
+        H2B1_Y,
+        H2B1_W,
+        H2B1_H,
+        H2B1_BGC
+    );
 
-  tft.setTextSize(H2B1_TEXT_SIZE);
-  tft.setTextDatum(MC_DATUM);
+    tft.drawRect(
+        H2B1_X,
+        H2B1_Y,
+        H2B1_W,
+        H2B1_H,
+        H2B1_BC
+    );
 
-  tft.setTextColor(
-    H2B1_TC,
-    H2B1_BGC
-  );
+    tft.setTextDatum(MC_DATUM);
+    tft.setTextColor(
+        H2B1_TC,
+        H2B1_BGC
+    );
 
-  tft.drawString(
-    "MENU",
-    H2B1_X + H2B1_W / 2,
-    H2B1_Y + H2B1_H / 2
-  );
+    tft.drawString(
+        "MENU",
+        H2B1_X + H2B1_W / 2,
+        H2B1_Y + H2B1_H / 2
+    );
 
-  // ----------------------------------------------------------
-  // H2B2 : TITRE
-  // ----------------------------------------------------------
+    // H2B2
+    tft.fillRect(
+        H2B2_X,
+        H2B2_Y,
+        H2B2_W,
+        H2B2_H,
+        H2B2_BGC
+    );
 
-  drawBlock(
-    H2B2_X,
-    H2B2_Y,
-    H2B2_W,
-    H2B2_H,
-    H2B2_BGC,
-    H2B2_BC
-  );
+    tft.drawRect(
+        H2B2_X,
+        H2B2_Y,
+        H2B2_W,
+        H2B2_H,
+        H2B2_BC
+    );
 
-  tft.setTextSize(H2B2_TEXT_SIZE);
+    tft.setTextColor(
+        H2B2_TC,
+        H2B2_BGC
+    );
 
-  tft.setTextColor(
-    H2B2_TC,
-    H2B2_BGC
-  );
+    tft.drawString(
+        title,
+        H2B2_X + H2B2_W / 2,
+        H2B2_Y + H2B2_H / 2
+    );
 
-  tft.drawString(
-    title,
-    H2B2_X + H2B2_W / 2,
-    H2B2_Y + H2B2_H / 2
-  );
+    // H2B3
+    tft.fillRect(
+        H2B3_X,
+        H2B3_Y,
+        H2B3_W,
+        H2B3_H,
+        H2B3_BGC
+    );
 
-  // ----------------------------------------------------------
-  // H2B3 : SETTINGS
-  // ----------------------------------------------------------
+    tft.drawRect(
+        H2B3_X,
+        H2B3_Y,
+        H2B3_W,
+        H2B3_H,
+        H2B3_BC
+    );
 
-  drawBlock(
-    H2B3_X,
-    H2B3_Y,
-    H2B3_W,
-    H2B3_H,
-    H2B3_BGC,
-    H2B3_BC
-  );
+    tft.setTextColor(
+        H2B3_TC,
+        H2B3_BGC
+    );
 
-  tft.setTextSize(H2B3_TEXT_SIZE);
+    tft.drawString(
+        "SET",
+        H2B3_X + H2B3_W / 2,
+        H2B3_Y + H2B3_H / 2
+    );
 
-  tft.setTextColor(
-    H2B3_TC,
-    H2B3_BGC
-  );
+    // Ligne basse
+    tft.drawFastHLine(
+        0,
+        H2_Y + H2_H - 1,
+        SCREEN_WIDTH,
+        H2_LINE_C
+    );
 
-  tft.drawString(
-    "*",
-    H2B3_X + H2B3_W / 2,
-    H2B3_Y + H2B3_H / 2
-  );
-
-  // ----------------------------------------------------------
-  // Ligne basse
-  // ----------------------------------------------------------
-
-  tft.drawFastHLine(
-    0,
-    H2_Y + H2_H - 1,
-    SCREEN_WIDTH,
-    H2_LINE_C
-  );
-
-  tft.setTextDatum(TL_DATUM);
+    tft.setTextDatum(TL_DATUM);
 }
 
-// ============================================================
-// H2B1 : DETECTION MENU
-// ============================================================
-
-bool header2MenuPressed(
-  int16_t x,
-  int16_t y
-)
+// H2B1 clic
+bool header2MenuPressed(int16_t x, int16_t y)
 {
-  return (
-    x >= H2B1_X &&
-    x < H2B1_X + H2B1_W &&
-    y >= H2B1_Y &&
-    y < H2B1_Y + H2B1_H
-  );
+    return (
+        x >= H2B1_X &&
+        x < H2B1_X + H2B1_W &&
+        y >= H2B1_Y &&
+        y < H2B1_Y + H2B1_H
+    );
 }
 
-// ============================================================
-// H2B3 : DETECTION SETTINGS
-// ============================================================
-
-bool header2SettingsPressed(
-  int16_t x,
-  int16_t y
-)
+// H2B3 clic
+bool header2SettingsPressed(int16_t x, int16_t y)
 {
-  return (
-    x >= H2B3_X &&
-    x < H2B3_X + H2B3_W &&
-    y >= H2B3_Y &&
-    y < H2B3_Y + H2B3_H
-  );
+    return (
+        x >= H2B3_X &&
+        x < H2B3_X + H2B3_W &&
+        y >= H2B3_Y &&
+        y < H2B3_Y + H2B3_H
+    );
 }
--- settings.h
+++ settings.h
@@ -3,101 +3,103 @@
 
 #include <TFT_eSPI.h>
 
-// 3x0c3t BO4RD v3.1 - SETTINGS
-
 // TFT
 #define TFT_ROTATION 2
-#define SCREEN_WIDTH  240
+#define SCREEN_WIDTH 240
 #define SCREEN_HEIGHT 320
 
-// Couleur générale
+// Couleurs
 #define COLOR_BACKGROUND TFT_BLACK
 
-// Header 1
+// HEADER 1
 #define H1_Y 0
 #define H1_H 10
 
-// H1B1 - Heure HH:MM:SS
+// H1B1
 #define H1B1_X 0
 #define H1B1_Y H1_Y
 #define H1B1_W 60
 #define H1B1_H H1_H
 #define H1B1_BGC TFT_GREEN
-#define H1B1_BC  TFT_GREEN
-#define H1B1_TC  TFT_BLACK
+#define H1B1_BC TFT_GREEN
+#define H1B1_TC TFT_BLACK
 #define H1B1_TEXT_SIZE 1
 
-// H1B2 - Date AAAA/MM/JJ
+// H1B2
 #define H1B2_X (H1B1_X + H1B1_W)
 #define H1B2_Y H1_Y
 #define H1B2_W 90
 #define H1B2_H H1_H
-#define H1B2_BGC TFT_GREEN
-#define H1B2_BC  TFT_GREEN
-#define H1B2_TC  TFT_BLACK
+#define H1B2_BGC TFT_BLACK
+#define H1B2_BC TFT_BLACK
+#define H1B2_TC TFT_GREEN
 #define H1B2_TEXT_SIZE 1
 
-// H1B3 - Logo -3X0C3T-
+// H1B3
 #define H1B3_X (H1B2_X + H1B2_W)
 #define H1B3_Y H1_Y
 #define H1B3_W 50
 #define H1B3_H H1_H
 #define H1B3_BGC TFT_BLACK
-#define H1B3_BC  TFT_GREEN
-#define H1B3_TC  TFT_RED
+#define H1B3_BC TFT_BLACK
+#define H1B3_TC TFT_RED
 #define H1B3_TEXT_SIZE 1
 
-// H1B4 - Status avec 2 carrés
+// H1B4
 #define H1B4_X (H1B3_X + H1B3_W)
 #define H1B4_Y H1_Y
-#define H1B4_W 40
+#define H1B4_W 20
 #define H1B4_H H1_H
 #define H1B4_BGC TFT_BLACK
-#define H1B4_BC  TFT_DARKGREY
-#define H1B4_TC  TFT_WHITE
+#define H1B4_BC TFT_DARKGREY
+#define H1B4_TC TFT_WHITE
+#define H1B4_TEXT_SIZE 1
 
-// H1B4 - Paramètres des carrés
-#define H1B4_SQUARE_SIZE     6
-#define H1B4_SQUARE_SPACING  4
-#define H1B4_SQUARE_COUNT    2
-#define H1B4_SQUARE_BGC TFT_BLACK
-#define H1B4_SQUARE_BC  TFT_DARKGREY
+// H1B5
+#define H1B5_X (H1B4_X + H1B4_W)
+#define H1B5_Y H1_Y
+#define H1B5_W 20
+#define H1B5_H H1_H
+#define H1B5_BGC TFT_BLACK
+#define H1B5_BC TFT_DARKGREY
+#define H1B5_TC TFT_WHITE
+#define H1B5_TEXT_SIZE 1
 
-// Header 2
+// HEADER 2
 #define H2_Y 10
 #define H2_H 20
 
-// H2B1 - Menu
+// H2B1
 #define H2B1_X 0
 #define H2B1_Y H2_Y
 #define H2B1_W 50
 #define H2B1_H H2_H
 #define H2B1_BGC 0x18E3
-#define H2B1_BC  TFT_DARKGREY
-#define H2B1_TC  TFT_WHITE
+#define H2B1_BC TFT_DARKGREY
+#define H2B1_TC TFT_WHITE
 #define H2B1_TEXT_SIZE 1
 
-// H2B3 - Settings
+// H2B3
 #define H2B3_W 50
 #define H2B3_H H2_H
 #define H2B3_X (SCREEN_WIDTH - H2B3_W)
 #define H2B3_Y H2_Y
 #define H2B3_BGC 0x18E3
-#define H2B3_BC  TFT_DARKGREY
-#define H2B3_TC  TFT_WHITE
+#define H2B3_BC TFT_DARKGREY
+#define H2B3_TC TFT_WHITE
 #define H2B3_TEXT_SIZE 1
 
-// H2B2 - Titre
+// H2B2
 #define H2B2_X (H2B1_X + H2B1_W)
 #define H2B2_Y H2_Y
 #define H2B2_W (SCREEN_WIDTH - H2B1_W - H2B3_W)
 #define H2B2_H H2_H
 #define H2B2_BGC TFT_BLACK
-#define H2B2_BC  TFT_DARKGREY
-#define H2B2_TC  TFT_WHITE
+#define H2B2_BC TFT_DARKGREY
+#define H2B2_TC TFT_WHITE
 #define H2B2_TEXT_SIZE 1
 
-// Header 2 - Ligne basse
+// Header 2 ligne
 #define H2_LINE_C TFT_DARKGREY
 
 // Contenu
@@ -112,21 +114,21 @@
 #define BUTTON_RADIUS 7
 #define BUTTON_SPACING 8
 
-// Couleurs des boutons
+// Couleurs boutons
 #define BUTTON_BGC 0x18E3
-#define BUTTON_BC  TFT_WHITE
-#define BUTTON_TC  TFT_WHITE
+#define BUTTON_BC TFT_WHITE
+#define BUTTON_TC TFT_WHITE
 
-// Carré gauche des boutons
+// Carré gauche
 #define BUTTON_LEFT_SQUARE_SIZE 18
 
-// Carrés droits des boutons
+// Carrés droite
 #define BUTTON_RIGHT_SQUARE_SIZE 12
-#define BUTTON_RIGHT_MARGIN      8
-#define BUTTON_RIGHT_SPACING     4
+#define BUTTON_RIGHT_MARGIN 8
+#define BUTTON_RIGHT_SPACING 4
 
-// Carré générique
+// Square générique
 #define SQUARE_BGC TFT_BLACK
-#define SQUARE_BC  TFT_DARKGREY
+#define SQUARE_BC TFT_DARKGREY
 
 #endif
